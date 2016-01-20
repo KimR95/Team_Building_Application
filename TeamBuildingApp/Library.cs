72c38588884f689bcd00022fd056e32ab22c514a
KimR95 <kimreid16@gmail.com>
2016-01-20 19:29:21 +0000

    EA Foundation Structure

--- TeamBuildingApp/Library.cs
+++ TeamBuildingApp/Library.cs
@@ -6,6 +6,9 @@ using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
 using System.IO;
 using System.Collections;
+using GAF;
+using GAF.Extensions;
+using GAF.Operators;
 
 namespace TeamBuildingApp
 {
@@ -15,6 +18,7 @@ namespace TeamBuildingApp
         private static Library instance;
         private static DBConnection dbC;
         private static List<Question> questions;
+       
         private static Student stud; 
 
         public static Library Instance
@@ -224,5 +228,175 @@ namespace TeamBuildingApp
         {
             instance = null;
         }
+
+        public void generateGroups(string classCode, int groupSize)
+        {
+            //get students from the db
+            List<Student>students = createStudents(classCode);
+
+            //create population from the available students
+            Population pop = createPopulation(students, groupSize);
+
+            //create operators & algorithm
+            createOperatorsAndAlgorithm(pop);
+
+
+
+        }
+
+        
+
+        private List<Student> createStudents(string classCode)
+        {
+            List<Student> students = new List<Student>();
+            MySqlCommand cmd = new MySqlCommand("SELECT * FROM STUDENTS WHERE CLASS_CODE = '" + classCode + "'", dbC.getConnection());
+            MySqlDataReader read = cmd.ExecuteReader();
+
+            while(read.Read())
+            {
+                students.Add(new Student(read.GetString("Student_no"), read.GetString("First_Name"), read.GetString("Second_Name"), classCode,
+                    read.GetInt32("Red_Results"), read.GetInt32("Blue_Results"), read.GetInt32("Green_Results"), read.GetInt32("Yellow_Results")));
+
+            }
+
+            return students;
+        }
+
+        private Population createPopulation(List<Student> students, int groupSize)
+        {
+            Population pop = new Population();
+
+            for (int i = 0; i < 100; i++)
+            {
+                Chromosome chromo = new Chromosome();
+
+                int j = 0;
+                var rnd = new Random();
+                students = students.OrderBy(x => rnd.Next()).ToList();
+
+                for (int k = 0; k <= students.Count; k++)
+                {
+                    if (j <= groupSize)
+                    {
+                        chromo.Genes.Add(new Gene(students[k]));
+                        j = chromo.Count;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                pop.Solutions.Add(chromo);
+            }
+
+            return pop;
+            
+
+        }
+
+        private void createOperatorsAndAlgorithm(Population pop)
+        {
+            Elite el = new Elite(5);
+
+            Crossover cr = new Crossover(0.7,false); 
+            cr.CrossoverType = CrossoverType.DoublePoint;
+
+            BinaryMutate mutate = new BinaryMutate(0.3, true);
+
+            GeneticAlgorithm genA = new GeneticAlgorithm(pop, calculateFitness);
+
+            genA.OnGenerationComplete += genA_OnGenerationComplete;
+            genA.OnRunComplete += genA_OnRunComplete;
+
+            //add the operators
+            genA.Operators.Add(el);
+            genA.Operators.Add(cr);
+            genA.Operators.Add(mutate);
+
+            //run the GA
+            genA.Run(genTerminate);
+
+       }
+
+       
+        private static double calculateFitness(Chromosome chromo, int red_allowance, int blue_allowance, int green_allowance, int yellow_allowance)
+        {
+            
+            int red = 0;
+            int blue = 0;
+            int green = 0;
+            int yellow =0;
+            int score = 100; 
+
+            //Count the instances of colours
+            foreach(Student stud in chromo)
+            {
+                switch(stud.getPrimary())
+                {
+                    case "Red": red += 1; break;
+                    case "Blue": blue += 1; break;
+                    case "Green": green += 1; break;
+                    case "Yellow": yellow += 1; break;
+                }
+            }
+
+            //balancing formula -- based on idea that one of each is perfect. 
+           
+            if(red > chromo.Count * (red_allowance/100))
+            {
+                //for every extra red a penalty of 4 - 4 being the worst penalty
+                score = score - (red - chromo.Count * (red_allowance / 100) * 4);
+            }
+
+            if (yellow > chromo.Count * (yellow_allowance / 100))
+            {
+                //for every extra red a penalty of 5 - 5 being the worst penalty
+                score = score - (yellow - chromo.Count * (yellow_allowance / 100) * 3);
+            }
+
+            if (blue > chromo.Count * (blue_allowance / 100))
+            {
+                //for every extra blue a penalty of 2
+                score = score - (blue - chromo.Count * (blue_allowance / 100) * 2);
+            }
+            
+            if (green > chromo.Count * (green_allowance / 100))
+            {
+                //for every extra blue a penalty of 2
+                score = score - (green - chromo.Count * (green_allowance / 100) * 2);
+            }
+
+            
+
+            
+
+
+
+
+            return 2.0; 
+        }
+
+        public static bool genTerminate(Population population, int currentGeneration, long currentEvaluation)
+        {
+            if (currentGeneration > 400)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        
+        private static void genA_OnRunComplete(object sender, GaEventArgs e)
+        {
+         
+        }
+
+        private static void genA_OnGenerationComplete(object sender, GaEventArgs e)
+        {
+         
+        }
+
+
     }
 }
---
+++ TeamBuildingApp/Question.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeamBuildingApp
+{
+    class Question
+    {
+        string id;
+        string question;
+        string redAnswer;
+        string blueAnswer;
+        string greenAnswer;
+        string yellowAnswer;
+
+        public Question(string ID,string q, string rA, string bA, string gA, string yA)
+        {
+            this.question = q;
+            this.redAnswer = rA;
+            this.blueAnswer = bA;
+            this.greenAnswer = gA;
+            this.yellowAnswer = yA;
+            this.id = ID;
+        }
+
+        public String getQuestion()
+        {
+            return this.question;
+        }
+
+        public String getRedAnswer()
+        {
+            return this.redAnswer;
+        }
+
+        public String getBlueAnswer()
+        {
+            return this.blueAnswer;
+        }
+
+        public String getGreenAnswer()
+        {
+            return this.greenAnswer;
+        }
+
+        public String getYellowAnswer()
+        {
+            return this.yellowAnswer;
+        }
+    }
+}
